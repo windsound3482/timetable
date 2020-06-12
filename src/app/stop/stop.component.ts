22fee2cb70e953b46854433f46313df275365be0
Windsound <a53745968@163.com>
2020-06-12 20:44:30 +0200

    zoom in and zoom out ready

--- src/app/stop/stop.component.ts
+++ src/app/stop/stop.component.ts
@@ -44,13 +44,10 @@ export class StopComponent implements OnInit {
     if (flag)
     {
       this.current=this.dataSource.length;
-      let tempint=Array(this.dataSource.length).fill("");
-      tempint[idindex]=this.value;
-      tempint[this.dataSource[0].indexOf("location_type")]=this.addtype;
-      this.dataSource.push(tempint);
+      this.addnewelemnt();
     }
-    let lonindex=this.displayedColumns.indexOf("stop_lon");
-    let latindex=this.displayedColumns.indexOf("stop_lat");
+    let lonindex=this.dataSource[0].indexOf("stop_lon");
+    let latindex=this.dataSource[0].indexOf("stop_lat");
     if (this.dataSource[this.current][lonindex])
     {
       this.map.flyTo({
@@ -67,21 +64,103 @@ export class StopComponent implements OnInit {
   }
 
   editparent(){
+    let idindex=this.dataSource[0].indexOf("stop_id");
+    for (var i=1;i<this.dataSource.length;i++)
+    {
+      if (this.dataSource[i][idindex]==this.currentparent)
+      {
+        this.currentparent=this.dataSource[i][this.dataSource[0].indexOf("parent_station")];
+        
+        this.value=this.currentparent;
+        this.getstation(this.currentparent);
+        let latindex=this.dataSource[0].indexOf("stop_lat");
+        let lonindex=this.dataSource[0].indexOf("stop_lon");
+       
+        this.map.flyTo({
+          center: [
+            parseFloat(this.dataSource[i][lonindex]),
+            parseFloat(this.dataSource[i][latindex])
+          ],
+          essential: true // this animation is considered essential with respect to prefers-reduced-motion
+        });
+        this.addmode=true;
+        this.editmode=false;
+        this.current=null;
+        break;
+      }
+    }
+  }
 
-    let temp=this.dataSource[this.current][this.displayedColumns.indexOf("parent_station")];
-    this.onSave();
-    this.value=temp;
+  editchild(){
+    let idindex=this.dataSource[0].indexOf("stop_id");
+    let flag=true;
+    for (var i=1;i<this.dataSource.length;i++)
+    {
+      if (this.dataSource[i][idindex]==this.value)
+      {
+        this.current=i;
+        flag=false;
+        break;
+      }
+    }
+    if (flag)
+    {
+      window.alert("not possible to edit a new added element");
+    }
+    else
+    {
+      let temptype=this.dataSource[this.current][this.dataSource[0].indexOf("location_type")];
+      if (temptype=="0" || temptype=="1")
+      {
+        this.currentparent=this.value;
+        
+        this.getstation(this.currentparent);
+        if (temptype=="0")
+        {
+          this.currenttype=["4"];
+          this.addtype="4";
+        }
+        if (temptype=="1")
+        {
+          this.currenttype=["0","2","3"];
+          this.addtype="0";
+        }
+        let latindex=this.dataSource[0].indexOf("stop_lat");
+        let lonindex=this.dataSource[0].indexOf("stop_lon");
+       
+        this.map.flyTo({
+          center: [
+            parseFloat(this.dataSource[this.current][lonindex]),
+            parseFloat(this.dataSource[this.current][latindex])
+          ],
+          essential: true // this animation is considered essential with respect to prefers-reduced-motion
+        });
+        this.value="";
+        this.addmode=true;
+        this.editmode=false;
+        this.current=null;
+      }
+    }
   }
 
   onSave(){
     this.stops.setstop(this.dataSource);
-    
     this.onReset();
   }
 
-  createElement(id,lat,lon){
+  createElement(id,lat,lon,type){
     var ell = document.createElement('div');
-        ell.innerHTML = "<span class=\"material-icons\">place</span>";
+    if (type=='0')
+      ell.innerHTML = " <span class=\"material-icons\" style=\"color:blue\">place</span>";
+    if (type=='1')
+      ell.innerHTML = " <span class=\"material-icons\" style=\"color:darkblue\">place</span>";
+    if (type=='2')
+      ell.innerHTML = " <span class=\"material-icons\" style=\"color:red\">place</span>";
+    if (type=='3')
+      ell.innerHTML = "<span class=\"material-icons\" style=\"color:brown\">place</span>";
+    if (type=='4')
+      ell.innerHTML = "<span class=\"material-icons\" style=\"color:gray\">place</span>";
+    
         ell.id = id;
         ell.addEventListener('click', () => 
           { 
@@ -105,7 +184,7 @@ export class StopComponent implements OnInit {
     this.onSave();
   }
 
-  getstation(type,parentstation){
+  getstation(parentstation){
     var tempmap=document.getElementById("map");
       tempmap.innerHTML=null;
       this.map=new mapboxgl.Map({
@@ -117,8 +196,8 @@ export class StopComponent implements OnInit {
       });
       this.map.addControl(new mapboxgl.NavigationControl());
      
-      let latindex=this.dataSource[0].indexOf("stop_lat");
-      let lonindex=this.dataSource[0].indexOf("stop_lon");
+    let latindex=this.dataSource[0].indexOf("stop_lat");
+    let lonindex=this.dataSource[0].indexOf("stop_lon");
      
     let index=[];
     for (var i=0;i<4;i++)
@@ -126,12 +205,12 @@ export class StopComponent implements OnInit {
       index.push(this.dataSource[0].indexOf(this.displayedColumns[i]));
     }
     this.database=[];
-    let typeindex=this.dataSource[0].indexOf("location_type");
     let parentindex=this.dataSource[0].indexOf("parent_station");
-   
+    let typeindex=this.dataSource[0].indexOf("location_type");
+    
       for (var i=1;i<this.dataSource.length;i++)
       {
-        if (((type==1) && !this.dataSource[i][parentindex]) || ((type!=1) && (this.dataSource[i][parentindex]==parentstation)))
+        if (this.dataSource[i][parentindex]==parentstation)
         {
           let tempinput=new Array(4).fill("");
           for (var j=0;j<4;j++)
@@ -140,22 +219,45 @@ export class StopComponent implements OnInit {
             }
           this.database.push(tempinput);
           if (this.dataSource[i][latindex] && this.dataSource[i][lonindex])
-           this.createElement(tempinput[0],this.dataSource[i][latindex].valueOf(),this.dataSource[i][lonindex].valueOf())
+           this.createElement(tempinput[0],this.dataSource[i][latindex].valueOf(),this.dataSource[i][lonindex].valueOf(),this.dataSource[i][typeindex]);
         }
       }
       this.dataTable=new MatTableDataSource<string[]>(this.database);
       this.dataTable.paginator = this.paginator;
 
-      if ((type==1) && (this.dataSource.length>1)) 
-      {
-        this.map.flyTo({
-          center: [
-            parseFloat(this.dataSource[1][lonindex]),
-            parseFloat(this.dataSource[1][latindex])
-          ],
-          essential: true // this animation is considered essential with respect to prefers-reduced-motion
+     
+      this.map.on('click', (e) => {
+      
+        if (this.editmode)
+        {
+          var el=document.getElementById(this.value);
+          if (el)
+            el.parentNode.removeChild( el );
+          this.createElement(this.value, e.lngLat.lat,e.lngLat.lng,this.addtype);
+          this.dataSource[this.current][this.dataSource[0].indexOf("stop_lat")]=e.lngLat.lat.toString();
+          this.dataSource[this.current][this.dataSource[0].indexOf("stop_lon")]=e.lngLat.lng.toString();
+          return;
+          
+        }
+        if (!this.addmode) 
+          {return;}
+        const dialogRef = this.dialog.open(InsertDialog, {
+          width: '250px',
         });
-      }
+        
+        dialogRef.afterClosed().subscribe(result => {
+          if (!result)
+          { window.alert("You need an ID to define the stop!!!");}
+          else{
+            this.createElement(result, e.lngLat.lat,e.lngLat.lng,this.addtype);
+            this.value=result;
+            this.addnewelemnt();
+            this.dataSource[this.dataSource.length-1][this.dataSource[0].indexOf("stop_lat")]=e.lngLat.lat.toString();
+            this.dataSource[this.dataSource.length-1][this.dataSource[0].indexOf("stop_lon")]=e.lngLat.lng.toString();
+          }
+        });
+       
+      }); 
   }
 
 
@@ -185,10 +287,28 @@ export class StopComponent implements OnInit {
         this.changecol();
       }
 
-      this.getstation(1,"");
+      this.getstation("");
+      let latindex=this.dataSource[0].indexOf("stop_lat");
+      let lonindex=this.dataSource[0].indexOf("stop_lon");
+      for (var i=1;i<this.dataSource.length;i++) 
+      {
+        if (this.dataSource[i][lonindex])
+        {
+          this.map.flyTo({
+            center: [
+              parseFloat(this.dataSource[i][lonindex]),
+              parseFloat(this.dataSource[i][latindex])
+            ],
+            essential: true // this animation is considered essential with respect to prefers-reduced-motion
+          });
+          break;
+        }
+      }
       this.currenttype=["0","1"];
       this.addtype="0";
-    }
+      this.currentparent="";
+    } 
+    
     this.value="";
     this.addmode=true;
     this.editmode=false;
@@ -199,48 +319,23 @@ export class StopComponent implements OnInit {
   addmode=true;
   editmode=false;
   addtype="0";
+  currentparent="";
   ngOnInit(): void {
     this.onReset();
-    this.map.on('click', (e) => {
-      
-      if (this.editmode)
-      {
-        var el=document.getElementById(this.value);
-        if (el)
-          el.parentNode.removeChild( el );
-        this.createElement(this.value, e.lngLat.lat,e.lngLat.lng);
-        this.dataSource[this.current][this.dataSource[0].indexOf("stop_lat")]=e.lngLat.lat.toString();
-        this.dataSource[this.current][this.dataSource[0].indexOf("stop_lon")]=e.lngLat.lng.toString();
-        return;
-        
-      }
-      if (!this.addmode) 
-        {return;}
-      const dialogRef = this.dialog.open(InsertDialog, {
-        width: '250px',
-      });
-      
-      dialogRef.afterClosed().subscribe(result => {
-        if (!result)
-        { window.alert("You need an ID to define the stop!!!");}
-        else{
-          this.createElement(result, e.lngLat.lat,e.lngLat.lng);
-          this.value=result;
-          let tempinput=new Array(this.dataSource[0].length).fill("");
-          tempinput[this.dataSource[0].indexOf("stop_id")]=this.value;
-          tempinput[this.dataSource[0].indexOf("stop_lat")]=e.lngLat.lat.toString();
-          tempinput[this.dataSource[0].indexOf("stop_lon")]=e.lngLat.lng.toString();
-          tempinput[this.dataSource[0].indexOf("location_type")]=this.addtype;
-          this.dataSource.push(tempinput);
-          this.database.push([this.value,"",this.addtype,""]);
-          this.dataTable=new MatTableDataSource<string[]>(this.database);
-          this.dataTable.paginator = this.paginator;
-        }
-      });
-     
-    }); 
+    
   }
- 
+  
+
+  addnewelemnt(){
+    let tempinput=new Array(this.dataSource[0].length).fill("");
+    tempinput[this.dataSource[0].indexOf("stop_id")]=this.value;
+    tempinput[this.dataSource[0].indexOf("location_type")]=this.addtype;
+    tempinput[this.dataSource[0].indexOf("parent_station")]=this.currentparent;
+    this.dataSource.push(tempinput);
+    this.database.push([this.value,"",this.addtype,this.currentparent]);
+    this.dataTable=new MatTableDataSource<string[]>(this.database);
+    this.dataTable.paginator = this.paginator;
+  }
   name: string;
  
   currenttype:string[]=[];
@@ -253,6 +348,12 @@ export class StopComponent implements OnInit {
       if (this.dataSource[0].includes(value[i])==false)
       {
         this.dataSource[0].push(value[i]);
+        if (value[i]=="location_type")
+          for (var j=1;j<this.dataSource.length;j++)
+            this.dataSource[j].push("0");
+        else
+          for (var j=1;j<this.dataSource.length;j++)
+            this.dataSource[j].push("");
         add=true;
         break;
       }
@@ -270,6 +371,10 @@ export class StopComponent implements OnInit {
       for (var j=0;j<this.dataSource.length;j++)
           this.dataSource[j].splice(tempindex,1);
     }
+  }
+
+  add_stop_input(input:string){
+    this.value=input;
   }
 }
 
